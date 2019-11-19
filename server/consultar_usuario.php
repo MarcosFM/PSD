e3f91ab0e02717708b2ef492c8403fbde6059a06
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-11-19 15:55:47 -0600

    Update consultar_usuario.php

--- server/consultar_usuario.php
+++ server/consultar_usuario.php
@@ -4,9 +4,9 @@ include 'conexion.php';
 echo "jala!";
 
 $consulta = "select * from heroku_b573b04d84016ce.users";
-
+echo "jala!2";
 mysqli_query($conexion, $consulta) or die (mysqli_error());
-echo "jala!";
+echo "jala!3";
 mysqli_close($conexion);
 
 ?>
