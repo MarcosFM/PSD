8c3644731cbbec6fd2ffdf8966a60e895082cff3
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-11-19 15:51:12 -0600

    Add files via upload

---
+++ server/consultar_usuario.php
@@ -0,0 +1,14 @@
+<?php
+include 'conexion.php';
+$username = $_POST['root'];
+$password = $_POST['root'];
+$nivel = $_POST['root'];
+$avatar = $_POST['root'];
+$cover = $_POST['root'];
+
+$consulta = "select * from heroku_b573b04d84016ce.users";
+
+mysqli_query($conexion, $consulta) or die (mysqli_error());
+mysqli_close($conexion);
+
+?>
