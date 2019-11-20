12407bfce963bfb2c1f094d07ccd4edb496487a5
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-11-19 20:58:49 -0600

    Update consultar_usuario.php

--- server/consultar_usuario.php
+++ server/consultar_usuario.php
@@ -5,7 +5,7 @@ $consulta = "select * from heroku_b573b04d84016ce.users";
 $resultado = $conexion -> query($consulta);
 
 $rows = array();
-while($r = mysqli_fetch_assoc($consulta)) {
+while($r = mysqli_fetch_assoc($resultado)) {
     $rows[] = $r;
 }
 echo json_encode($rows);
