8a2f00e3bb143e75d629a60d082284cd1a69fd04
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-12-05 07:37:11 -0600

    Update insertar_usuario.php

--- server/insertar_usuario.php
+++ server/insertar_usuario.php
@@ -5,11 +5,11 @@ $password = $_POST['password'];
 $nivel = $_POST['nivel'];
 $avatar = $_POST['avatar'];
 $cover = $_POST['cover'];
+echo ("done");
 
 $consulta = "insert into heroku_b573b04d84016ce.users (username,password,nivel,avatar,cover) 
 VALUES ('".$username."','".$password."','".$nivel."','".$avatar."','".$cover."')";
 
 mysqli_query($conexion, $consulta) or die (mysqli_error());
 mysqli_close($conexion);
-echo ("done");
 ?>
