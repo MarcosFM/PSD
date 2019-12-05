c239d099ba1fa3895140b5c2f76d5450d66cb8c7
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-12-05 07:31:54 -0600

    Add files via upload

--- server/insertar_usuario.php
+++ server/insertar_usuario.php
@@ -1,9 +1,14 @@
 <?php
 include 'conexion.php';
-$username = $_POST['root'];
-$password = $_POST['root'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$nivel = $_POST['nivel'];
+$avatar = $_POST['avatar'];
+$cover = $_POST['cover'];
 
-$consulta = "insert into heroku_b573b04d84016ce.users (username,passord) values ('".$username."','".$password."')";
+$consulta = "insert into heroku_b573b04d84016ce.users (username,password,nivel,avatar,cover) 
+VALUES ('".$username."','".$password."','".$nivel."','".$avatar."','".$cover."')";
+
 mysqli_query($conexion, $consulta) or die (mysqli_error());
 mysqli_close($conexion);
 ?>
