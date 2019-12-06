9e50ca920970c77fa05e945f8bef3c5dc385ecef
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-12-06 00:20:31 -0600

    Update insertar_usuario.php

--- server/insertar_usuario.php
+++ server/insertar_usuario.php
@@ -5,13 +5,18 @@ $password = $_POST['password'];
 $nivel = $_POST['nivel'];
 $avatar = $_POST['avatar'];
 $cover = $_POST['cover'];
-echo ("done");
 
-//$consulta = "insert into heroku_b573b04d84016ce.users (username,password,nivel,avatar,cover) 
-//VALUES ('".$username."','".$password."','".$nivel."','".$avatar."','".$cover."')";
+$username = !empty($username) ? $username : null;
+$password = !empty($password) ? $password : null;
+$nivel = !empty($nivel) ? $nivel : null;
+$avatar = !empty($avatar) ? $avatar : null;
+$cover = !empty($cover) ? $cover : null;
 
 $consulta = "insert into heroku_b573b04d84016ce.users (username, password, nivel, avatar, cover) 
-VALUES ('marcos','molina','', null , null)";
+VALUES ('".$username."','".$password."','".$nivel."','".$avatar."','".$cover."')";
+
+//$consulta = "insert into heroku_b573b04d84016ce.users (username, password, nivel, avatar, cover) 
+//VALUES ('marcos','molina','', null , null)";
 
 if (mysqli_query($conexion, $consulta)){ 
   echo "Nuevo registro exitoso";
