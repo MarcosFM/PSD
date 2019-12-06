495b89bdbe7a32ca56af843355f8600203d28d8d
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-12-05 22:34:24 -0600

    Update insertar_usuario.php

--- server/insertar_usuario.php
+++ server/insertar_usuario.php
@@ -7,9 +7,17 @@ $avatar = $_POST['avatar'];
 $cover = $_POST['cover'];
 echo ("done");
 
-$consulta = "insert into heroku_b573b04d84016ce.users (username,password,nivel,avatar,cover) 
-VALUES ('".$username."','".$password."','".$nivel."','".$avatar."','".$cover."')";
+//$consulta = "insert into heroku_b573b04d84016ce.users (username,password,nivel,avatar,cover) 
+//VALUES ('".$username."','".$password."','".$nivel."','".$avatar."','".$cover."')";
 
-mysqli_query($conexion, $consulta) or die (mysqli_error());
+$consulta = "insert into heroku_b573b04d84016ce.users (username, password, nivel, avatar, cover) 
+VALUES ('marcos','molina','', null , null)";
+
+if (mysqli_query($conexion, $consulta)){ 
+  echo "Nuevo registro exitoso";
+} else{
+  echo "Error: " .$consulta. "<br>".mysqli_error($conexion);
+}
+
 mysqli_close($conexion);
 ?>
