fca147219b85aa02d73c14c282b4b89b5d5cecbe
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-12-06 20:42:27 -0600

    Update insertar_usuario.php

--- server/insertar_usuario.php
+++ server/insertar_usuario.php
@@ -2,18 +2,12 @@
 include 'conexion.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
-$nivel = $_POST['nivel'];
-$avatar = $_POST['avatar'];
-$cover = $_POST['cover'];
 
 $username = !empty($username) ? $username : null;
 $password = !empty($password) ? $password : null;
-$nivel = !empty($nivel) ? $nivel : null;
-$avatar = !empty($avatar) ? $avatar : null;
-$cover = !empty($cover) ? $cover : null;
 
-$consulta = "insert into heroku_b573b04d84016ce.users (username, password, nivel, avatar, cover) 
-VALUES ('".$username."','".$password."','".$nivel."','".$avatar."','".$cover."')";
+$consulta = "insert into heroku_b573b04d84016ce.users (username, password) 
+VALUES ('".$username."','".$password."')";
 
 //$consulta = "insert into heroku_b573b04d84016ce.users (username, password, nivel, avatar, cover) 
 //VALUES ('marcos','molina','', null , null)";
