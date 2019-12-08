84a58c7d3ae4659955e134759a4aac3e597de408
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-12-08 15:19:20 -0600

    Update consulta.php

--- server/consulta.php
+++ server/consulta.php
@@ -3,7 +3,7 @@ include 'conexion.php';
 $fk = $_GET['fk'];
 
 $consulta = "SELECT + FROM heroku_b573b04d84016ce.contador WHERE fk = '".$fk."'";
-$resultado = $consulta -> query($consulta);
+$resultado = $conexion -> query($consulta);
 
 while ($fila = $resultado -> fetch_array()){
   $desafio[] = array_map('utf8_encode', $fila);
