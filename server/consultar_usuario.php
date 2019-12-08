44fc85cd21f5e8227e0371f4baac5e69620844dd
Marcos Molina <32813649+MarcosFM@users.noreply.github.com>
2019-12-08 13:32:25 -0600

    Update consultar_usuario.php

--- server/consultar_usuario.php
+++ server/consultar_usuario.php
@@ -1,7 +1,7 @@
 <?php
 include 'conexion.php';
 
-$consulta = "select * from heroku_b573b04d84016ce.users";
+$consulta = "select * from heroku_b573b04d84016ce.contador";
 $resultado = $conexion -> query($consulta);
 $rows = array();
 while($r = mysqli_fetch_assoc($resultado)) {
